ede8408a35799fb8aa23dd8d7dff21850231ab24
Michel Adão Teixeira <mateixe@gmail.com>
2024-12-16 23:41:03 -0300

    primeiro exercicio

---
+++ exercise1/Notes/SharedResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(128, 0, 128);
+        public static readonly Color BackgroundColor = Color.FromRgb(208, 113, 255);
+    }
+}
